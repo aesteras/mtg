43aa55c02ff12184b756e42964add5c4012bd271
Andreu Esteras <26363123+aesteras@users.noreply.github.com>
2026-02-24 11:33:24 +0100

    run prettier

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
-import {ref} from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -20,7 +20,7 @@ const count = ref(0)
 	<p>
 		Check out
 		<a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-		>create-vue</a
+			>create-vue</a
 		>, the official Vue + Vite starter
 	</p>
 	<p>
@@ -28,7 +28,7 @@ const count = ref(0)
 		<a
 			href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
 			target="_blank"
-		>Vue Docs Scaling up Guide</a
+			>Vue Docs Scaling up Guide</a
 		>.
 	</p>
 	<p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
