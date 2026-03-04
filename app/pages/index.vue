dd1e65deffb76ced7eb506ecfec9993b5c84c9ef
aesteras <26363123+aesteras@users.noreply.github.com>
2026-03-04 13:21:15 +0100

    nuxt content wip

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+const { data: decksInfo } = await useAsyncData("authors", () => {
+	return queryCollection("decksInfo").order("name", "DESC").all();
+});
+
+console.log({ decksInfo });
+</script>
+
 <template>
 	<UContainer>
 		<p>This is a body</p>
---
+++ content.config.ts
@@ -0,0 +1,33 @@
+import { defineCollection, defineContentConfig } from "@nuxt/content";
+import { z } from "zod";
+
+export default defineContentConfig({
+	collections: {
+		// Decks Info
+		decksInfo: defineCollection({
+			type: "data",
+			source: "decks/**/*.json",
+			schema: z
+				.object({
+					id: z.int(),
+					deckName: z.string(),
+					image: z.string(),
+					colors: z.string(),
+				})
+				.transform((deck) => ({
+					...deck,
+					archidektUrl: `https://archidekt.com/decks/${deck.id}`,
+				})),
+		}),
+
+		// Decks Results
+		decksResults: defineCollection({
+			type: "data",
+			source: "decks/**/*.csv",
+			schema: z.object({
+				date: z.iso.date(),
+				result: z.regex(/^[LWlw]+$/),
+			}),
+		}),
+	},
+});
