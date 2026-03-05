5924afef35da15ed562c94e94d50209483f390eb
Andreu Esteras <26363123+aesteras@users.noreply.github.com>
2026-03-05 22:49:19 +0100

    sort placeholder selector and switch

--- app/components/DeckGrid.vue
+++ app/components/DeckGrid.vue
@@ -2,13 +2,29 @@
 import { useDeckData } from "~/composables/useDeckData";
 
 const deckData = useDeckData();
+
+const sortItems = ref([
+	"Total Played",
+	"Total Wins",
+	"Total Losses",
+	"Won Percentage",
+	"Lost Percentage",
+]);
+const sortValue = ref("Total Played");
+const reverse = ref(false);
+
 deckData.value = deckData.value.sort(
 	(first, second) => first.totalPlayed - second.totalPlayed
 );
 </script>
 
 <template>
-	<UPageGrid>
+	<div class="flex flex-row items-center">
+		<p class="text-sm mr-2">Sort:</p>
+		<USelect v-model="sortValue" class="mr-2" :items="sortItems" />
+		<USwitch v-model="reverse" label="Reversed" />
+	</div>
+	<UPageGrid class="pt-4">
 		<div v-for="(data, index) in deckData" :key="index">
 			<DeckCard :data="data" />
 		</div>
