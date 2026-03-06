89e1aa10a6fd956cbb76e81aec6e2e5fa283ca9a
aesteras <26363123+aesteras@users.noreply.github.com>
2026-03-06 11:00:13 +0100

    sort first impl pending bug fix

--- app/components/DeckGrid.vue
+++ app/components/DeckGrid.vue
@@ -1,27 +1,44 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { useDeckData } from "~/composables/useDeckData";
+import type { DeckData } from "~~/types/deckData";
 
 const deckData = useDeckData();
 
-const sortItems = ref([
-	"Total Played",
-	"Total Wins",
-	"Total Losses",
-	"Won Percentage",
-	"Lost Percentage",
-]);
+const sortFieldMap: Record<string, keyof DeckData> = {
+	"Total Played": "totalPlayed",
+	"Total Wins": "totalWins",
+	"Total Losses": "totalLosses",
+	"Won Percentage": "wonPercentage",
+	"Lost Percentage": "lostPercentage",
+} as const;
+const sortItems = ref(Object.keys(sortFieldMap));
 const sortValue = ref("Total Played");
 const reverse = ref(false);
 
-deckData.value = deckData.value.sort(
-	(first, second) => first.totalPlayed - second.totalPlayed
-);
+const sortDecks = (
+	fieldKey: keyof typeof sortFieldMap,
+	reverse: boolean = false
+) => {
+	const field = sortFieldMap[fieldKey]!;
+	deckData.value = deckData.value.sort((left, right) => {
+		const leftVal = Number(left[field]);
+		const rightVal = Number(right[field]);
+		if (reverse) return rightVal - leftVal;
+		return leftVal - rightVal;
+	});
+};
+
+// Set up watcher
+watch([sortValue, reverse], () => sortDecks(sortValue.value, reverse.value));
+
+// Initial sort
+sortDecks("Total Played");
 </script>
 
 <template>
 	<div class="flex flex-row items-center">
 		<p class="text-sm mr-2">Sort:</p>
-		<USelect v-model="sortValue" class="mr-2" :items="sortItems" />
+		<USelect v-model="sortValue" :items="sortItems" class="mr-2" />
 		<USwitch v-model="reverse" label="Reversed" />
 	</div>
 	<UPageGrid class="pt-4">
