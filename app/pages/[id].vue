50a05ecd4746b0d837b6f392e0ee603d390732ed
Andreu Esteras <26363123+aesteras@users.noreply.github.com>
2026-07-15 22:43:45 +0200

    deck details poc

---
+++ app/pages/[id].vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useDecksData } from "~/composables/useDecksData";
+
+const { params } = useRoute();
+const decksData = await useDecksData();
+const deckData = decksData.value.find(
+	(deck) => String(deck.archidektId) === params.id
+);
+</script>
+
+<template>
+	<div v-if="deckData === undefined">No data</div>
+	<div v-else>
+		<p>archidektId: {{ deckData.archidektId }}</p>
+		<p>deckName: {{ deckData.deckName }}</p>
+		<p>image: {{ deckData.image }}</p>
+		<p>colors: {{ deckData.colors }}</p>
+		<p>id: {{ deckData.id }}</p>
+		<p>results: {{ deckData.results }}</p>
+		<p>totalWins: {{ deckData.totalWins }}</p>
+		<p>totalLosses: {{ deckData.totalLosses }}</p>
+		<p>totalPlayed: {{ deckData.totalPlayed }}</p>
+		<p>wonPercentage: {{ deckData.wonPercentage }}</p>
+		<p>lostPercentage: {{ deckData.lostPercentage }}</p>
+	</div>
+</template>
