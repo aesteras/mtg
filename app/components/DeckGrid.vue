9c73762dd6efae55d4ad216347c3e62fcb4a4fd4
Andreu Esteras <26363123+aesteras@users.noreply.github.com>
2026-07-15 22:52:51 +0200

    dynamic switch label

--- app/components/DeckGrid.vue
+++ app/components/DeckGrid.vue
@@ -8,7 +8,7 @@ const sortFieldMap: Record<string, keyof DeckData> = {
 	"Games Played": "totalPlayed",
 	"Games Won": "totalWins",
 	"Games Lost": "totalLosses",
-	"Win/Loss Ratio": "wonPercentage",
+	"Win Ratio": "wonPercentage",
 } as const;
 const sortItems = ref(Object.keys(sortFieldMap));
 const sortValue = ref("Games Played");
@@ -38,7 +38,7 @@ sortDecks("Games Played");
 	<div class="flex flex-row items-center">
 		<p class="text-sm mr-2">Sort:</p>
 		<USelect v-model="sortValue" :items="sortItems" class="mr-2" />
-		<USwitch v-model="reverse" label="Reversed" />
+		<USwitch v-model="reverse" :label="reverse ? 'Descending' : 'Ascending'" />
 	</div>
 	<UPageGrid class="pt-4">
 		<div v-for="data in deckData" :key="data.id">
