ec659545fed6a7876143df969dcfc0bf727936b6
Andreu Esteras <26363123+aesteras@users.noreply.github.com>
2026-07-16 20:55:39 +0200

    deck header

---
+++ app/components/HeroHeader.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+defineProps<{
+	title: string;
+	image: string;
+}>();
+</script>
+
+<template>
+	<header class="relative h-48 md:h-64 overflow-hidden">
+		<img
+			:src="image"
+			alt="Deck Image"
+			:class="[
+				'absolute',
+				'inset-0',
+				'w-full',
+				'h-full',
+				'object-cover',
+				'scale-110',
+				'blur-xs',
+				'lg:blur-md',
+				'opacity-50',
+				'lg:left-auto',
+				'lg:right-0',
+				'lg:w-[66%]',
+				'hero-mask',
+			]"
+		>
+
+		<div
+			class="absolute inset-0 bg-black/30 lg:left-auto lg:right-0 lg:w-[66%] hero-mask"
+		/>
+
+		<UContainer class="relative h-full flex items-end pb-8">
+			<h1 class="text-4xl font-bold text-white">
+				{{ title }}
+			</h1>
+		</UContainer>
+	</header>
+</template>
+
+<style scoped>
+.hero-mask {
+	mask-image: none;
+	-webkit-mask-image: none;
+}
+
+@media (min-width: 64rem) {
+	.hero-mask {
+		mask-image: linear-gradient(to left, black 33%, transparent);
+		-webkit-mask-image: linear-gradient(to left, black 33%, transparent);
+	}
+}
+</style>
--- app/pages/[id].vue
+++ app/pages/[id].vue
@@ -11,16 +11,20 @@ const deckData = decksData.value.find(
 <template>
 	<div v-if="deckData === undefined">No data</div>
 	<div v-else>
-		<p>archidektId: {{ deckData.archidektId }}</p>
-		<p>deckName: {{ deckData.deckName }}</p>
-		<p>image: {{ deckData.image }}</p>
-		<p>colors: {{ deckData.colors }}</p>
-		<p>id: {{ deckData.id }}</p>
-		<p>results: {{ deckData.results }}</p>
-		<p>totalWins: {{ deckData.totalWins }}</p>
-		<p>totalLosses: {{ deckData.totalLosses }}</p>
-		<p>totalPlayed: {{ deckData.totalPlayed }}</p>
-		<p>wonPercentage: {{ deckData.wonPercentage }}</p>
-		<p>lostPercentage: {{ deckData.lostPercentage }}</p>
+		<HeroHeader :title="deckData.deckName" :image="deckData.image" />
+		<UContainer>
+			<!--placeholder data-->
+			<p>archidektId: {{ deckData.archidektId }}</p>
+			<p>deckName: {{ deckData.deckName }}</p>
+			<p>image: {{ deckData.image }}</p>
+			<p>colors: {{ deckData.colors }}</p>
+			<p>id: {{ deckData.id }}</p>
+			<p>results: {{ deckData.results }}</p>
+			<p>totalWins: {{ deckData.totalWins }}</p>
+			<p>totalLosses: {{ deckData.totalLosses }}</p>
+			<p>totalPlayed: {{ deckData.totalPlayed }}</p>
+			<p>wonPercentage: {{ deckData.wonPercentage }}</p>
+			<p>lostPercentage: {{ deckData.lostPercentage }}</p>
+		</UContainer>
 	</div>
 </template>
